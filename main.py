e42aeefb733d7b211e58cf8998ba2764ea25ec0a
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-05-18 14:31:43 +0000

    finished

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+def main():
+    Number = int(userInput())
+    deno = -1
+    Pi = 0
+    for i in range(Number):
+        deno = deno + 2
+        if i % 2 == 0: #if i is positive
+            Pi = Pi + 4/deno
+        else:
+            Pi = Pi - 4/deno
+    print(Pi)
+def userInput():
+    while True:
+        Num = input()
+        if Num.isnumeric():
+            if int(Num) > 0:
+                return Num
+
+main()
